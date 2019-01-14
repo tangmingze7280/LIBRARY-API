e7d657e1df2a73a927f2991c654c211ee5d380ce
tang <17853537280@163.com>
2019-01-14 16:32:50 +0800

    spring-boot整合
    Signed-off-by: tang <17853537280@163.com>

--- src/main/java/com/sbmybatis/wbapps/service/interf/UserService.java
+++ src/main/java/com/sbmybatis/wbapps/service/interf/UserService.java
@@ -3,7 +3,7 @@ package com.sbmybatis.wbapps.service.interf;
 import com.sbmybatis.wbapps.entity.User;
 
 public interface UserService {
-    public User getUserById(Integer userId);
+    User getUserById(Integer userId);
 
     boolean addUser(User record);
 }
