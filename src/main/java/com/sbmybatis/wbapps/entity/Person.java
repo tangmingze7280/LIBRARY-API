1ea570b8d20536e38e381504df1db98176c86905
tang <17853537280@163.com>
2019-03-03 16:09:49 +0800

    测试 从yml文件中读键值对 并且给成员变量赋值

--- src/main/java/com/sbmybatis/wbapps/entity/Person.java
+++ src/main/java/com/sbmybatis/wbapps/entity/Person.java
@@ -1,15 +1,36 @@
 package com.sbmybatis.wbapps.entity;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
+@Component
 @ConfigurationProperties(prefix = "person")
 public class Person {
     private String lastName;
     private int age;
     private String[] wtf;
     private List<Object> lit;
+    private boolean flag;
+    private Date time;
+
+    public boolean isFlag() {
+        return flag;
+    }
+
+    public void setFlag(boolean flag) {
+        this.flag = flag;
+    }
+
+    public Date getTime() {
+        return time;
+    }
+
+    public void setTime(Date time) {
+        this.time = time;
+    }
 
     public String getLastName() {
         return lastName;
@@ -38,6 +59,8 @@ public class Person {
                 ", age=" + age +
                 ", wtf=" + Arrays.toString(wtf) +
                 ", lit=" + lit +
+                ", flag=" + flag +
+                ", time=" + time +
                 '}';
     }
 
--- src/test/java/com/sbmybatis/wbapps/WbappsApplicationTests.java
+++ src/test/java/com/sbmybatis/wbapps/WbappsApplicationTests.java
@@ -1,17 +1,20 @@
 package com.sbmybatis.wbapps;
 
+import com.sbmybatis.wbapps.entity.Person;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class WbappsApplicationTests {
-
+    @Autowired
+    Person person;
     @Test
     public void contextLoads() {
-        System.out.println("1111111");
+        System.out.println(person);
     }
 
 }
