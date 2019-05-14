b98f09caea5bfe389798c1ee25ea312fdc9c2e89
唐鸣泽丨 <oncwnuck7itnt5auadkvhns9pxrk@git.weixin.qq.com>
2019-05-14 13:10:21 +0800

    CXK，5缺一

--- src/main/java/com/sbmybatis/wbapps/config/HttpsConfig.java
+++ src/main/java/com/sbmybatis/wbapps/config/HttpsConfig.java
@@ -2,11 +2,13 @@ package com.sbmybatis.wbapps.config;
 
 import org.apache.catalina.Context;
 import org.apache.catalina.connector.Connector;
+import org.apache.coyote.http11.Http11NioProtocol;
 import org.apache.tomcat.util.descriptor.web.SecurityCollection;
 import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
 import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -30,11 +32,27 @@ public class HttpsConfig {
     }
 
     private Connector getHttpConnector() {
+//        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+//        connector.setScheme("http");
+//        connector.setPort(80);
+//        connector.setSecure(false);
+//        connector.setRedirectPort(443);
+//        return connector;
+
         Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-        connector.setScheme("http");
-        connector.setPort(80);
-        connector.setSecure(false);
-        connector.setRedirectPort(443);
+        Http11NioProtocol protocol = (Http11NioProtocol) connector.getProtocolHandler();
+
+        connector.setPort(443);
+        connector.setScheme("https");
+        connector.setSecure(true);
+        protocol.setSSLEnabled(true);
+        protocol.setClientAuth("false");
+        protocol.setSSLProtocol("TLSv1+TLSv1.1+TLSv1.2");
+        protocol.setKeystoreFile("classpath:sslhttps.jks");
+        protocol.setKeystorePass("152821b95f451");
+        protocol.setKeyAlias("www.gttview.club");
+        protocol.setKeystoreType("JKS");
+        protocol.setSSLEnabled(true);
         return connector;
     }
     public void customize(ConfigurableEmbeddedServletContainer container) {
